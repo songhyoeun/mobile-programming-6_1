34f7a1cd83486c24c27a6f3f48e7339991b9f197
songhyoeun <166578433+songhyoeun@users.noreply.github.com>
2024-10-20 21:18:26 +0900

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.selfproject6_1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.graphics.Color;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.DatePicker;
+import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Chronometer chrono;
+    RadioButton rdoCal, rdoTime;
+    DatePicker dPicker;
+    TimePicker tPicker;
+    TextView tvYear, tvMonth, tvDay, tvHour, tvMin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("시간 예약");
+
+        chrono = (Chronometer) findViewById(R.id.chronometer1);
+
+        rdoCal = (RadioButton) findViewById(R.id.rdoCal);
+        rdoTime = (RadioButton) findViewById(R.id.rdoTime);
+
+        dPicker = (DatePicker) findViewById(R.id.datePicker1);
+        tPicker = (TimePicker) findViewById(R.id.timePicker1);
+
+        tvYear = (TextView) findViewById(R.id.tvYear);
+        tvMonth = (TextView) findViewById(R.id.tvMonth);
+        tvDay = (TextView) findViewById(R.id.tvDay);
+        tvHour = (TextView) findViewById(R.id.tvHour);
+        tvMin = (TextView) findViewById(R.id.tvMin);
+
+        rdoCal.setVisibility(View.INVISIBLE);
+        rdoTime.setVisibility(View.INVISIBLE);
+        dPicker.setVisibility(View.INVISIBLE);
+        tPicker.setVisibility(View.INVISIBLE);
+
+        rdoCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tPicker.setVisibility(View.INVISIBLE);
+                dPicker.setVisibility(View.VISIBLE);
+            }
+        });
+
+        rdoTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tPicker.setVisibility(View.VISIBLE);
+                dPicker.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        chrono.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chrono.setBase(SystemClock.elapsedRealtime());
+                chrono.start();
+                chrono.setTextColor(Color.RED);
+                rdoCal.setVisibility(View.VISIBLE);
+                rdoTime.setVisibility(View.VISIBLE);
+            }
+        });
+
+        tvYear.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                chrono.stop();
+                chrono.setTextColor(Color.BLUE);
+
+                tvYear.setText(Integer.toString(dPicker.getYear()));
+                tvMonth.setText(Integer.toString(1 + dPicker.getMonth()));
+                tvDay.setText(Integer.toString(dPicker.getDayOfMonth()));
+
+                tvHour.setText(Integer.toString(tPicker.getCurrentHour()));
+                tvMin.setText(Integer.toString(tPicker.getCurrentMinute()));
+
+                rdoCal.setVisibility(View.INVISIBLE);
+                rdoTime.setVisibility(View.INVISIBLE);
+                tPicker.setVisibility(View.INVISIBLE);
+                dPicker.setVisibility(View.INVISIBLE);
+                return false;
+            }
+        });
+    }
+}
